a3a050a815b5844c6f171239180c161dda1af9c6
Victor Kreniski <krevi2796@gmail.com>
2020-08-28 11:39:09 -0300

    Created Models to Request Data from iTunes aPI

--- BookSeeker/ViewController.swift
+++ BookSeeker/ViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let sessionProvider = URLSessionProvider()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        sessionProvider.request(type: ITunesResponse.self,
+                                service: ItunesBookService.eBookSearch(term: "Swift")) { (response) in
+            switch response {
+            case .success(let iTunesResponseData):
+                print(iTunesResponseData)
+            case .failure(let networkError):
+                print("error: \(networkError)")
+            }
+        }
     }
 }
